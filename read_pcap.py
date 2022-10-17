e573f36b294e03dbb9f406d38182a72db4da04ac
Larry Kuo <larrykuo.tech@gmail.com>
2022-10-18 03:50:23 +0800

    myParser.py first version

--- read_pcap.py
+++ read_pcap.py
@@ -2,54 +2,49 @@ from binascii import hexlify
 from scapy.all import *
 import binascii
 import re
-
+# from myParser import Parser
 
 
 # print( a )
 # print( len(a[0][1]))
 pkt = rdpcap('mms_cap1.pcapng')
-print( "1.", bytes(pkt[0]))
-print("2.", binascii.hexlify(bytes(pkt[0])).decode())
+print( "1.", pkt[0] )
+content = binascii.hexlify(bytes(pkt[0])).decode()
+print( "2.", content )
 
-
-
-def Read( pkt ) :
-
+def Read_and_Parse_Encapsulation( pkt ):
+    print( "1.", pkt[0] )
     content = binascii.hexlify(bytes(pkt[0])).decode()
-
-    cotp_index = re.search('0300....02f080', content).span() # search COTP
-
-    print(cotp_index)
-    print(len(content))
-    content = content[108:]
-    print( 'test1', content )
-
-    end = False
-    list_temp = []
-    start_index = 0
-    while ( start_index != len(content) ) :
-        print(start_index)
-        tkpt_length = int(content[start_index+4:start_index+8], 16)*2
-        tpkt_payload = content[start_index:start_index+tkpt_length]
-        list_temp.append(tpkt_payload)
-        start_index += tkpt_length
-        
-        
-def ParseEncapsulate( content ):
-
+    print( "2.", content )
     
-    TPKT = [content[:2], content[2:4], content[4:8]]
+    cotp_index = re.search('0300....02f080', content).span() # search COTP
+    content = content[cotp_index[0]:]
+    
+    start_index = 0
+    tkpt_length = int(content[start_index+4:start_index+8], 16)*2
+    tpkt_payload = content[start_index:start_index+tkpt_length]
+    
+    TPKT = [tpkt_payload[:2], tpkt_payload[2:4], tpkt_payload[4:8]]
     print("TKPT", TPKT)
-    COTP = [content[8:10], content[10:12], content[12:14]]
+    COTP = [tpkt_payload[8:10], tpkt_payload[10:12], tpkt_payload[12:14]]
     print("COTP", COTP)
-    ISO8327A = [content[14:16], content[16:18]]
+    ISO8327A = [tpkt_payload[14:16], tpkt_payload[16:18]]
     print("ISO8327A",ISO8327A)
-    ISO8327B = [content[18:20], content[20:22]]
+    ISO8327B = [tpkt_payload[18:20], tpkt_payload[20:22]]
     print("ISO8327B",ISO8327B)
-    print("is 61", content[22:24])
-    ISO8823 = []
+    
+    print("ISO8823 begin with 61", tpkt_payload[22:24])
+    
+    print("ISO8823 payload", tpkt_payload[24:] )
+    
+    # ISO8823, rest = Parser( tpkt_payload[22:], "ISO8823" ) 
+    # MMS, rest = Parser( rest, "MMS" ) 
         
-# Read(pkt)
+
+
+Read_and_Parse_Encapsulation(pkt)
+
+
 
 # print(scapy_cap.summary())
 # print(scapy_cap.hexraw())

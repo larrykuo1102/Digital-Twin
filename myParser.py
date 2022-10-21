8380a060bf92060b3e977df610c3951284ca032a
Larry Kuo <larrykuo.tech@gmail.com>
2022-10-21 23:43:47 +0800

    add some function and test

--- myParser.py
+++ myParser.py
@@ -108,16 +108,22 @@ def MMS_Parser( value : str, mms_data : list ) :
     data, rest = ASN1_parser(value)
     if data['tag'] == 'a0' :
         temp_list = []
-        temp_dict['confirmed_RequestPDU'] = temp_list
-        rest = confirmed_RequestPDU( data['value'], temp_list )
+        temp_dict['Confirmed_RequestPDU'] = temp_list
+        rest = Confirmed_RequestPDU( data['value'], temp_list )
     elif data['tag'] == 'a1' :
-        rest = confirmed_ResponsePDU( data['value'] ) 
+        temp_list = []
+        temp_dict['Confirmed_ResponsePDU'] = temp_list
+        rest = Confirmed_ResponsePDU( data['value'], temp_list ) 
+    elif data['tag'] == 'a3' :
+        temp_list = []
+        temp_dict['Unconfirmed_ResponsePDU'] = temp_list
+        rest = Unconfirmed_ResponsePDU( data['value'], temp_list ) 
     return rest
     
 def ISO8823_Parser( value : str ) :
     data, rest = ASN1_parser(value)
     if data['tag'] == 'a0' :
-        confirmed_RequestPDU( data['value'] )
+        Confirmed_RequestPDU( data['value'] )
     elif data['tag'] == 'a1' :
         rest = confirmed_ResponsePDU( data['value'] ) 
     
@@ -125,7 +131,7 @@ def ISO8823_Parser( value : str ) :
     return rest
 
 
-def confirmed_RequestPDU( value : str, mms_data : list ) :
+def Confirmed_RequestPDU( value : str, mms_data : list ) :
     temp_dict = {}
     mms_data.append( temp_dict )
     
@@ -138,6 +144,10 @@ def confirmed_RequestPDU( value : str, mms_data : list ) :
         temp_list = []
         temp_dict['Write_Request'] = temp_list
         rest = Write_Request( data['value'], temp_list )
+    elif ( data['tag'] == 'a4') : # read
+        temp_list = []
+        temp_dict['Read_Request'] = temp_list
+        rest = Read_Request( data['value'], temp_list )
     elif ( data['tag'] == '81' ) :
         pass
     
@@ -154,7 +164,19 @@ def Write_Request( value : str, mms_data : list ) :
         rest = VariableAccessSpecification( data['value'], temp_list )
     
     return rest
-        
+
+def Read_Request( value : str, mms_data : list) :
+    temp_dict = {}
+    mms_data.append( temp_dict)
+
+    data, rest = ASN1_parser(value)
+    if (data['tag'] == 'a0') :
+        temp_list =[]
+        temp_dict['VariableAccessSpecification'] =temp_list
+        rest = VariableAccessSpecification( data['value'], temp_list)
+
+    return rest
+
 def VariableAccessSpecification( value : str, mms_data : list ) :
     temp_dict = {}
     mms_data.append( temp_dict )
@@ -201,8 +223,28 @@ def ObjectName( value : str, mms_data : list ) :
     return rest
     
 
-def confirmed_ResponsePDU(  value : str, mms_data : list ) :
-    pass
+def confirmed_ResponsePDU( value : str, mms_data : list ) :
+    temp_dict = {}
+    mms_data.append( temp_dict )
+    
+    data, rest = ASN1_parser(value)
+    if ( data['tag'] == '02') : # invokeID
+        temp_dict['invokeID'] = data['value']
+        
+    data, rest = ASN1_parser(rest)
+    if ( data['tag'] == 'a5' ) : # write
+        temp_list = []
+        temp_dict['Write_Response'] = temp_list
+        rest = Write_Response( data['value'], temp_list )
+    elif ( data['tag'] == 'a4') : # read
+        temp_list = []
+        temp_dict['Read_Response'] = temp_list
+        rest = Read_Response( data['value'], temp_list )
+    elif ( data['tag'] == '81' ) :
+        pass
+    
+    return rest
+
 
 def Parser( content : str, protocol : str ) -> list : 
     MMS_data :list = []
@@ -221,12 +263,10 @@ def Parser( content : str, protocol : str ) -> list :
     elif ( protocol == 'GOOSE') :
         pass
     
-    
 
+test_input =   "a9ffa082012202020d8ea482011aa1820116a08201123029a027a1251a0b41514632353552656c61791a16456e657267794d4d54523124535424546f74576824743029a027a1251a0b41514632353552656c61791a16456e657267794d4d54523124535424546f7457682471302ea02ca12a1a0b41514632353552656c61791a1b456e657267794d4d54523124535424546f7457682461637456616c302ba029a1271a0b41514632353552656c61791a18456e657267794d4d54523124535424546f74564172682474302ba029a1271a0b41514632353552656c61791a18456e657267794d4d54523124535424546f745641726824713030a02ea12c1a0b41514632353552656c61791a1d456e657267794d4d54523124535424546f74564172682461637456616c"
+# test_input = "a962a06302020d90a55da02a3028a026a1241a0b41514632353552656c61791a154f424a33435357493324434f24506f732453424f77a02fa22d830100a214850103890f454c495053452d49454336313835308601069108000000000000000a83010084020600"
 # test_input = "a962a0600202021ba55aa0273025a023a1211a0a5245463632304354524c1a134342435357493124434f24506f732453424f77a02fa22d830101a214850103890f454c495053452d49454336313835308601009108000000000000000a83010084020600"    
-# Parser(test_input, "MMS")
-
-
-
-
+a = Parser(test_input, "MMS")
+print(a)
 

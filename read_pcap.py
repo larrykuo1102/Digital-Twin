c9caa64cd9b8f76c07fced66f5fd3feeb171e2ba
Larry Kuo <larrykuo.tech@gmail.com>
2022-10-23 00:54:48 +0800

    rebase from main

--- read_pcap.py
+++ read_pcap.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import binascii
 import re
 
@@ -24,37 +23,11 @@ def Read_and_Parse_Encapsulation(pkt):
     tkpt_length = int(content[start_index+4:start_index+8], 16)*2
     tpkt_payload = content[start_index:start_index+tkpt_length]
 
-=======
-from binascii import hexlify
-from scapy.all import *
-import binascii
-import re
-# from myParser import Parser as myParser
-
-pkt = rdpcap('mms_cap1.pcapng')
-print( "1.", pkt[0] )
-content = binascii.hexlify(bytes(pkt[0])).decode()
-print( "2.", content )
-
-def Read_and_Parse_Encapsulation( pkt ):
-    print( "1.", pkt[0] )
-    content = binascii.hexlify(bytes(pkt[0])).decode()
-    print( "2.", content )
-    
-    cotp_index = re.search('0300....02f080', content).span() # search COTP
-    content = content[cotp_index[0]:]
-    
-    start_index = 0
-    tkpt_length = int(content[start_index+4:start_index+8], 16)*2
-    tpkt_payload = content[start_index:start_index+tkpt_length]
-    
->>>>>>> ce2eaf57285881434448a77bc7161990493e4001
     TPKT = [tpkt_payload[:2], tpkt_payload[2:4], tpkt_payload[4:8]]
     print("TKPT", TPKT)
     COTP = [tpkt_payload[8:10], tpkt_payload[10:12], tpkt_payload[12:14]]
     print("COTP", COTP)
     ISO8327A = [tpkt_payload[14:16], tpkt_payload[16:18]]
-<<<<<<< HEAD
     print("ISO8327A", ISO8327A)
     ISO8327B = [tpkt_payload[18:20], tpkt_payload[20:22]]
     print("ISO8327B", ISO8327B)
@@ -76,25 +49,9 @@ print("ISO8823 payload", tpkt_payload[24:])
 # ISO8823, rest = Parser( tpkt_payload[22:], "ISO8823" )
 # MMS, rest = Parser( rest, "MMS" )
 
-=======
-    print("ISO8327A",ISO8327A)
-    ISO8327B = [tpkt_payload[18:20], tpkt_payload[20:22]]
-    print("ISO8327B",ISO8327B)
-    
-    print("ISO8823 begin with 61", tpkt_payload[22:24])
-    
-    print("ISO8823 payload", tpkt_payload[24:] )
-    
-    # ISO8823, rest = Parser( tpkt_payload[22:], "ISO8823" ) 
-    # MMS, rest = Parser( rest, "MMS" ) 
->>>>>>> ce2eaf57285881434448a77bc7161990493e4001
 
 Read_and_Parse_Encapsulation(pkt)
 
 # all_packets = sniff(offline = 'abb es_open ds_open cb_open2close_pcap.pcap', filter='dst host 192.168.2.13', prn=lambda x: x.summary() ) # 效率好
 # print(all_packets)
-<<<<<<< HEAD
 # for per_packet in all_packets :
-=======
-# for per_packet in all_packets :
->>>>>>> ce2eaf57285881434448a77bc7161990493e4001
